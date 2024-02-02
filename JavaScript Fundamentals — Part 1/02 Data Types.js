b0280818f7fd0fd8c17a8378260e0927369eb410
shatha790 <108197490+shatha790@users.noreply.github.com>
2024-02-02 23:26:53 +0300

    Create 02 Data Types.js

---
+++ JavaScript Fundamentals — Part 1/02 Data Types.js
@@ -0,0 +1,16 @@
+/* 
+Declare a variable called isIsland and set its value according to your country. 
+The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+Log the types of isIsland, population, country and language to the console.
+*/
+
+let isIsland =false;
+let country="Saudi Arabia"
+let population=35.95;
+let language;
+
+console.log(typeof isIsland); //Boolean 
+console.log(typeof population); //number
+console.log(typeof country); //string
+console.log(typeof language); //undefined
