77009f15ce683f536b0ba9626878803197add493
shatha790 <108197490+shatha790@users.noreply.github.com>
2024-02-02 23:07:10 +0300

    Update 01 Values and Variables.js

--- JavaScript Fundamentals — Part 1/01 Values and Variables.js
+++ JavaScript Fundamentals — Part 1/01 Values and Variables.js
@@ -1,1 +1,13 @@
+/*
+Declare variables called country, continent and population and assign their values according 
+to your own country (population in millions).
 
+Log their values to the console.
+*/
+let country="Saudi Arabia";
+let continent="Asia";
+let population="35.95";
+
+console.log(country);
+console.log(continent);
+console.log(population);
