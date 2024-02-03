06bb4624672f3e8a4bbb4045a0ca18cdd114302d
shatha790 <108197490+shatha790@users.noreply.github.com>
2024-02-03 22:55:12 +0300

    Create 03 let, const and var.js

---
+++ JavaScript Fundamentals — Part 1/03 let, const and var.js
@@ -0,0 +1,18 @@
+/* 
+Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+
+Think about which variables should be const variables (which values will never change, and which might change?).
+Then, change these variables to const.
+
+Try to change one of the changed variables now, and observe what happens.
+*/
+
+let language;
+language= "Arabic"
+
+const country="Saudi Arabia"
+const continent = 'Asia';
+const isIsland = false;
+isIsland = true;
+//show error: 
+// typeError:Assignments to constant variables
