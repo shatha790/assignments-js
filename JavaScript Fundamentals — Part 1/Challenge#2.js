18ab9bad5b6157806450f574f605d562d93179aa
shatha790 <108197490+shatha790@users.noreply.github.com>
2024-02-04 10:00:45 +0300

    Create Challenge#2.js

---
+++ JavaScript Fundamentals — Part 1/Challenge#2.js
@@ -0,0 +1,18 @@
+ const marksWeights1=78;
+ const johnWeights1= 92;
+
+ const marksHeight1=1.69;
+ const johnHeight1= 1.95;
+
+const marksBMI=marksWeights1 / marksHeight1 ** 2;
+const johnBMI=johnWeights1 / johnHeight1 ** 2; 
+
+const markHigherBMI= marksHeight1>johnHeight1;
+
+if (marksWeights1>johnWeights1){
+  console.log(`Mark's
+BMI ${marksWeights1} is higher than John's ${johnWeights1}`);
+}else{
+console.log(`John's ${johnWeights1} is higher than Mark's
+BMI ${marksWeights1}`); 
+}
