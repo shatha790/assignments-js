ae4f92b5b00e44eea9164a83a8329db901ad9d36
shatha790 <108197490+shatha790@users.noreply.github.com>
2024-02-04 09:34:03 +0300

    Create  Challenge#1.js

---
+++ JavaScript Fundamentals — Part 1/Challenge#1.js
@@ -0,0 +1,19 @@
+ const marksWeights1=78;
+ const johnWeights1= 92;
+
+ const marksHeight1=1.69;
+ const johnHeight1= 1.95;
+
+ const marksWeights2=95;
+ const johnWeights2=85;
+
+ const marksHeight2=1.88;
+ const johnHeight2=1.76;
+
+const marksBMI=marksWeights1 / marksHeight1 ** 2;
+const johnBMI=johnWeights1 / johnHeight1 ** 2; 
+
+const markHigherBMI= marksHeight1>johnHeight1;
+
+console.log(marksBMI,johnBMI);
+console.log(markHigherBMI);
