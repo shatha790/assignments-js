0d070d9383f78383aa9e98ac4d23a626d6bc57df
shatha790 <108197490+shatha790@users.noreply.github.com>
2024-02-07 23:04:38 +0300

    Create 03 Arrow Functions.js

---
+++ JavaScript Fundamentals — Part 2/03 Arrow Functions.js
@@ -0,0 +1,9 @@
+/*
+Recreate the last assignment, but this time create an arrow function called percentageOfWorld3.
+*/
+
+//Arrow Functions
+percentageOfWolrd3= (population)=>{
+ return (population/7900)*100
+ }
+console.log(percentageOfWolrd3(10));
