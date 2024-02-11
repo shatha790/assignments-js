55d1e6767fa441fdf83605fd7ffd9c4fc34d9dea
shatha790 <108197490+shatha790@users.noreply.github.com>
2024-02-11 23:26:24 +0300

    Create 07 Introduction to Objects.js

---
+++ JavaScript Fundamentals — Part 2/07 Introduction to Objects.js
@@ -0,0 +1,13 @@
+/* 
+1- Create an object called myCountry for a country of your choice, containing properties country, capital, language,
+population and neighbours (an array like we used in previous assignments).
+
+*/
+
+const myCountry={
+ country : 'Saudi Arabia',
+  capital:'Riyadh',
+   language:'Arabic',
+ population:35,
+ neighbours :["Riyadh","Jeddah","Madinah"]
+};
