040dca4e4c4234f0faacd9f2295cf3bb38a045dc
shatha790 <108197490+shatha790@users.noreply.github.com>
2024-02-26 22:03:54 +0300

    Create Coding-Challenge-#2.js

---
+++ Data Structures, Modern Operators and Strings/Coding-Challenge-#2.js
@@ -0,0 +1,58 @@
+const game = {
+team1: 'Bayern Munich',
+team2: 'Borrussia Dortmund',
+players: [
+[
+'Neuer',
+'Pavard',
+'Martinez',
+'Alaba',
+'Davies',
+'Kimmich',
+'Goretzka',
+'Coman',
+'Muller',
+'Gnarby',
+'Lewandowski',
+],
+[
+'Burki',
+'Schulz',
+'Hummels',
+'Akanji',
+'Hakimi',
+'Weigl',
+'Witsel',
+'Hazard',
+'Brandt',
+'Sancho',
+'Gotze',
+],
+],
+score: '4:0',
+scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+'Hummels'],
+date: 'Nov 9th, 2037',
+odds: {
+team1: 1.33,
+x: 3.25,
+team2: 6.5,
+},
+};
+//1
+for(const[i,el] of game.scored.entries() ){
+console.log( `Goal ${i+1}: ${el}`)
+}
+//2
+let sum=0;
+const values = Object.values(game.odds)
+for(const odd of values){
+sum +=odd;
+}
+let ave=sum/values.length;
+console.log(ave);
+//3
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
